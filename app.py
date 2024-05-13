d109157fc426998a8c4a6aaa900bb8438345a5d4
Jean Carlo Herran <75239077+JeanC221@users.noreply.github.com>
2024-05-12 22:47:18 -0500

    app.py

--- app.py
+++ app.py
@@ -1,15 +1,33 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from routes.user import lib
 from tags import tags_metadata
+import uvicorn
 # uvicorn app:app --reload
 
 app = FastAPI(
-    title="MongoDB CRUD Bilioteca",
-  description="Manejo de prestamo en biblioteca",
-  version="0.0.1",
-  openapi_tags=tags_metadata
-
+    title="MongoDB CRUD Biblioteca",
+    description="Manejo de préstamo en biblioteca",
+    version="0.0.1",
+    openapi_tags=tags_metadata
 )
 
-#incluir ruta
+# Incluir ruta de usuario
 app.include_router(lib)
+
+# Servir archivos estáticos
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Esto permite todas las orígenes en desarrollo
+    allow_credentials=True,
+    allow_methods=["*"],  
+    allow_headers=["*"],  
+)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000)
